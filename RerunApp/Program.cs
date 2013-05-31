b419b621a6844acdd64dadf589f51684de7bf47c
Serkan Berksoy <serkan.berksoy@yahoo.com>
2013-05-31 12:04:07 +0300

    First Commit

---
+++ RerunApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RerunApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2 || string.IsNullOrEmpty(args[0]) || string.IsNullOrEmpty(args[1]))
+            {
+                Console.WriteLine(@"Rerun Process
+Usage: RERUNPROCESS.EXE [EXECUTABLENAME] [PATH] 
+");
+                return;
+            }
+            
+            Process thisProc = Process.GetCurrentProcess();
+            if (!IsProcessOpen(args[0].ToLower().Replace(".exe", "")))
+            {
+                StartProcess(args);
+            }
+            else
+            {
+                Console.WriteLine("{0} Process Is Already Running", args[0]);
+            }
+        }
+
+        private static void StartProcess(string[] args)
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = Path.Combine(args[1], args[0]);
+            try
+            {
+                p.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error Rerunning {0}\n{1}", Path.Combine(args[1], args[0]), ex.Message);
+            }
+        }
+
+        public static bool IsProcessOpen(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.ToLower().Contains(name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
